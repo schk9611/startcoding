5e1aec990c08336f896e3d974b20088fe6373226
Sony <schk9611@gmail.com>
2022-06-08 12:58:45 +0900

    update pyautogui in project1.py

--- project1.py
+++ project1.py
@@ -1,13 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+# pyautogui 불러오기
+import pyautogui
 
+# pyautogui의 prompt 함수를 통해 입력창 띄워 입력 받기
+keyword = pyautogui.prompt("검색어를 입력하세요.")
+
+# f string을 사용해 문자열 안 변수 사용하기
 response = requests.get(
-    "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90")
+    f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={keyword}")
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
-# soup에 저장된 html 중 news_tit 클래스를 가진 태그를 모두 가져와 저장
-links = soup.select(".news_tit")  # select 함수는 가져온 값들을 리스트로 저장한다
+links = soup.select(".news_tit")
 for link in links:
-    title = link.text  # 태그 안에 텍스트 요소를 가져온다
-    url = link.attrs['href']  # href의 속성값을 가져온다
+    title = link.text
+    url = link.attrs['href']
     print(title, url)
