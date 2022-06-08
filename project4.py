fb2dae1899a804d05ebd24e5665b3abaf706f15f
Sony <schk9611@gmail.com>
2022-06-08 16:12:33 +0900

    add project4

---
+++ project4.py
@@ -0,0 +1,20 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# 크롬 드라이버 자동 업데이트
+from webdriver_manager.chrome import ChromeDriverManager
+
+# 브라우저 꺼짐 방지
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# 불필요한 에러 메세지 없애기
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+# 웹 페이지 해당 주소 이동
+driver.get("https://www.naver.com")
